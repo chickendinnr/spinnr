d09f9b22b42a6389f2d24bffb798ec6df0076193
RJ McBain <33466125+rjmcbain@users.noreply.github.com>
2018-05-21 18:34:35 -0600

    Delete env.js

--- env.js
+++
@@ -1,7 +0,0 @@
-const clientId =
-  "137036059505-v4nntigb8q1qnqs0gerrs3gktm9bem9m.apps.googleusercontent.com";
-const clientSecret = "ZzAlOMbcZc-UuDOf_Pkgc8-R";
-const refreshToken = "1/T_E28TkTfS37WwzB3h5zHQcdqChtLEumgfEitsUZcqM";
-module.exports.clientId = clientId;
-module.exports.clientSecret = clientSecret;
-module.exports.refreshToken = refreshToken;
