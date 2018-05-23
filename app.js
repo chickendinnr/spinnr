12f0006cba1970ca222c04f8fada548e039017e8
Matt Steffey <Matt@Matts-MacBook-Pro.local>
2018-05-23 10:48:20 -0600

    test

--- app.js
+++ app.js
@@ -221,4 +221,7 @@ app.post("/", (req, res) => {
   });
 });
 
-app.listen(3000, () => console.log("Server Running"));
+var server = app.listen(process.env.PORT || 3000, function () {
+  var port = server.address().port;
+  console.log("Express is working on port " + port);
+});
