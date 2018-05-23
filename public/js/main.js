1ecf1d89ac71ab9ebc8d69920596a97eb49425ec
Matt Steffey <Matt@Matts-MacBook-Pro.local>
2018-05-23 15:15:08 -0600

    test

--- public/js/main.js
+++ public/js/main.js
@@ -68,8 +68,9 @@ let spotifyKey = "";
 async function go() {
   // first ask for get user media
 
-  const stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } }).then(function success(stream) {
-        video.srcObject = stream;
+
+const stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } });
+  video.srcObject = stream;
 }
 
 
