5e95ec97f414e6b05f03991883b92f83aa9d5041
rjmcbain <rmcbain01@gmail.com>
2018-06-02 14:02:31 -0600

    adding social links and mailing list

--- public/js/main.js
+++ public/js/main.js
@@ -1,164 +1,148 @@
-
-
 // Initialize Firebase
 
-// var config = {
-//   apiKey: "AIzaSyCxJuTlAg7uaZgViNAO8GtP3691ZYMHUrY",
-//   authDomain: "contactform-f3f52.firebaseapp.com",
-//   databaseURL: "https://contactform-f3f52.firebaseio.com",
-//   projectId: "contactform-f3f52",
-//   storageBucket: "contactform-f3f52.appspot.com",
-//   messagingSenderId: "866809098065"
-// };
-// firebase.initializeApp(config);
+var config = {
+  apiKey: "AIzaSyCOhqovHOeRVuCwdZaXk6rV5zFKQoooAXc",
+  authDomain: "mailinglist-b4c1e.firebaseapp.com",
+  databaseURL: "https://mailinglist-b4c1e.firebaseio.com",
+  projectId: "mailinglist-b4c1e",
+  storageBucket: "",
+  messagingSenderId: "533131973511"
+};
+firebase.initializeApp(config);
 
-// // Reference messages collection
-// let messagesRef = firebase.database().ref("messages");
+// Reference messages collection
+let messagesRef = firebase.database().ref("messages");
 
-// document.getElementById("contactForm").addEventListener("submit", submitForm);
+document.getElementById("contactForm").addEventListener("submit", submitForm);
 
-// function submitForm(e) {
-//   e.preventDefault();
+function submitForm(e) {
+  e.preventDefault();
 
-//   // Get Values
-//   let email = getInputVal("email");
+  // Get Values
+  let email = getInputVal("email");
 
-//   // Save message
+  // Save message
 
-//   saveMessage(email);
+  saveMessage(email);
 
-//   // Show alert
-//   document.querySelector(".alert").style.display = "block";
+  // Show alert
+  document.querySelector(".alert").style.display = "block";
 
-//   // Reset form
-//   document.getElementById("contactForm").reset();
-// }
+  // Reset form
+  document.getElementById("contactForm").reset();
+}
 
-// // Function to get form values
+// Function to get form values
 
-// function getInputVal(id) {
-//   return document.getElementById(id).value;
-// }
+function getInputVal(id) {
+  return document.getElementById(id).value;
+}
 
-// // Save message to firebase
+// Save message to firebase
 
-// function saveMessage(email) {
-//   let newMessageRef = messagesRef.push();
-//   newMessageRef.set({
-//     email: email
-//   });
-// }
-
-
-
-
-
-
+function saveMessage(email) {
+  let newMessageRef = messagesRef.push();
+  newMessageRef.set({
+    email: email
+  });
+}
 
 //////////THIS IS THE CAMERA AND API CALL CODE
 
-
-
-const video = document.querySelector('.handsome');
-const canvas = document.querySelector('#paint');
-const ctx = canvas.getContext('2d');
+const video = document.querySelector(".handsome");
+const canvas = document.querySelector("#paint");
+const ctx = canvas.getContext("2d");
 let spotifyKey = "";
-
 
 async function go() {
   // first ask for get user media
 
-
-const stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } });
+  const stream = await navigator.mediaDevices.getUserMedia({
+    video: { facingMode: "environment" }
+  });
   video.srcObject = stream;
 }
-
 
 function takePhoto() {
   canvas.width = video.videoWidth;
   canvas.height = video.videoHeight;
   ctx.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
-  const data = canvas.toDataURL('image/png');
+  const data = canvas.toDataURL("image/png");
   var res = data.replace("data:image/png;base64,", "");
- 
 
-
- var type = "WEB_DETECTION";
- var json = '{' +
+  var type = "WEB_DETECTION";
+  var json =
+    "{" +
     ' "requests": [' +
-    ' { ' +
+    " { " +
     '   "image": {' +
-    '     "content":"' + res + '"' +
-    '   },' +
+    '     "content":"' +
+    res +
+    '"' +
+    "   }," +
     '   "features": [' +
-    '       {' +
-    '         "type": "' + type + '",' +
+    "       {" +
+    '         "type": "' +
+    type +
+    '",' +
     '     "maxResults": 1' +
-    '       }' +
-    '   ]' +
-    ' }' +
-    ']' +
-    '}';
+    "       }" +
+    "   ]" +
+    " }" +
+    "]" +
+    "}";
 
-$.ajax({
-    type: 'POST',
-    url: "https://vision.googleapis.com/v1/images:annotate?key=AIzaSyD1h9v1qDj0EMx6UjLtpFn5t0ZjPGxstSQ",
-    dataType: 'json',
+  $.ajax({
+    type: "POST",
+    url:
+      "https://vision.googleapis.com/v1/images:annotate?key=AIzaSyD1h9v1qDj0EMx6UjLtpFn5t0ZjPGxstSQ",
+    dataType: "json",
     data: json,
     //Include headers, otherwise you get an odd 400 error.
     headers: {
-      "Content-Type": "application/json",
+      "Content-Type": "application/json"
     },
 
     success: function(data, textStatus, jqXHR) {
-      var access_token = window.location.href.split('=').join('&').split('&');
+      var access_token = window.location.href
+        .split("=")
+        .join("&")
+        .split("&");
       spotifyKey = access_token[1];
 
       musicInfo = data.responses["0"].webDetection.bestGuessLabels["0"].label;
       $.ajax({
         method: "GET",
-        url: "https://api.spotify.com/v1/search?q=" + musicInfo + "&type=album&limit=1",
+        url:
+          "https://api.spotify.com/v1/search?q=" +
+          musicInfo +
+          "&type=album&limit=1",
         headers: {
-        Accept: 'application/json',
-        Authorization: 'Bearer '+ spotifyKey 
-},
-          }).done(function(data) {
-             window.open(data.albums.items["0"].external_urls.spotify,'_blank');
-             console.log(data.albums.items["0"]);
+          Accept: "application/json",
+          Authorization: "Bearer " + spotifyKey
+        }
+      }).done(function(data) {
+        window.open(data.albums.items["0"].external_urls.spotify, "_blank");
+        console.log(data.albums.items["0"]);
 
-
-
-
-             // var albumURL = document.getElementById('player');
-             // albumURL.setAttribute("src", 'https://open.spotify.com/embed?uri='+ data.albums.items["0"].uri);
-             // console.log(data.albums.items["0"].uri);
-
-          });
+        // var albumURL = document.getElementById('player');
+        // albumURL.setAttribute("src", 'https://open.spotify.com/embed?uri='+ data.albums.items["0"].uri);
+        // console.log(data.albums.items["0"].uri);
+      });
 
       console.log(musicInfo);
-
-  },
+    },
     error: function(jqXHR, textStatus, errorThrown) {
-      console.log('ERRORS: ' + textStatus + ' ' + errorThrown);
+      console.log("ERRORS: " + textStatus + " " + errorThrown);
     }
-  });   
-
-
-    }
-
+  });
+}
 
 function deletePhoto() {
   var can = document.getElementById("paint");
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-
 }
 
 go().catch(err => {
   alert(err.message);
 });
-
-
-
-
-
-
