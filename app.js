c7fe17851e6a6a6cdb38aaf0183cc5531e0a76b8
Matt Steffey <Matt@Matts-MacBook-Pro.local>
2018-06-06 10:38:02 -0600

    tessssst

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.get("/", (req, res) => {
 
 
 
-app.get("/#/", (req, res) => {
+app.get("/#", (req, res) => {
   res.render("demo.html");
 });
 
@@ -143,14 +143,15 @@ app.get("/callback", function(req, res) {
 
         // we can also pass the token to the browser to make requests from there
         res.redirect(
-          "/#/" +
+          "/#" +
             querystring.stringify({
               access_token: access_token,
               refresh_token: refresh_token
             }));
+         res.render("demo.html");
       } else {
         res.redirect(
-          "/#/" +
+          "/#" +
             querystring.stringify({
               error: "invalid_token"
             })
